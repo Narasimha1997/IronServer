863ba723b913085f893eaa6c289843d2db7256d4
Narasimha1997 <yogaimpression@gmail.com>
2019-08-03 20:23:15 +0530

    First Commit

--- test.py
+++ test.py
@@ -4,22 +4,30 @@ import matplotlib.pyplot as plt
 
 URL = "http://localhost:8888/static/js/main.30fffb9d.chunk.js.map"
 
-URL2 = "http://localhost:5000/static/main.30fffb9d.chunk.js.map"
+URL2 = "http://localhost:5000/static/js/main.30fffb9d.chunk.js.map"
+
+times2 = []
+for i in range(100):
+
+    st = time.time()
+    data = requests.get(URL2)
+    et = time.time() - st
+    times2.append(et)
 
 times = []
 
-for i in range(191):
+for i in range(100):
 
     st = time.time()
     data = requests.get(URL)
-
-    print(data.headers, data.status_code)
     et = time.time() - st
     times.append(et)
 
 
-
+print('Total Time : ')
+print("Node.js serve , 1000 requests : ", str(sum(times2)) + " seconds") 
 print("Fast Web Server 1000 requests : ", str(sum(times)) + " seconds")
 
-plt.plot(times)
+plt.plot(times, label = "Fast C server")
+plt.plot(times2, label = "Node.js serve")
 plt.show()
